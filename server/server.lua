5e253746139a546837f12004dc21888e4528edfc
CristianoTRS <50265039+CristianoTRS@users.noreply.github.com>
2020-05-19 23:29:46 +0200

    Create server.lua

---
+++ server/server.lua
@@ -0,0 +1,13 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('staticAction:server:actionCommand')
+AddEventHandler('staticAction:server:actionCommand', function(coords, args, name)
+    TriggerClientEvent('staticAction:client:actionCommand', -1, coords, args, name)
+end)
+
+RegisterNetEvent('staticAction:server:ractionCommand')
+AddEventHandler('staticAction:server:ractionCommand', function(source, args)
+    TriggerClientEvent('staticAction:client:ractionCommand', -1, source, args)
+end)
